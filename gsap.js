33e15bdd1316d4dd1149bb1d258532a9fc9b5bd8
shahzaib-mahmood <shahzaibmehmood027@gmail.com>
2024-05-12 04:52:19 -0700

    modification

---
+++ gsap.js
@@ -0,0 +1,32 @@
+gsap.to(".box" ,{
+    x:1000,
+    y:200,
+    duration:2,
+    delay:1,
+    backgroundColor:"blue",
+    borderRadius:"50%",
+    rotate:360,
+    // skewY:10
+    // skewY:10
+    scale:1.5,
+    repeat:-1,
+    yoyo:true
+    
+})
+
+// gsap.from(".box2", {
+//     x:900,
+//     y:900,
+//     duration:1.5,
+//     delay:1,
+// })
+
+// gsap.from("h1" ,{
+//     // color: "red",
+//     opacity:0,
+//     duration:1,
+//     x:-20,
+//     delay:1,
+//     stagger:0.3
+
+// })
